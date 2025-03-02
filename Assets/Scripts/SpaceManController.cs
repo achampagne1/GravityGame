4dc7f5f011b39f34ea95904d0b2482206ceea537
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2025-03-02 13:02:50 -0600

    added animation controller

--- Assets/Scripts/SpaceManController.cs
+++ Assets/Scripts/SpaceManController.cs
@@ -5,12 +5,15 @@ using UnityEngine.InputSystem;
 
 public class SpaceManController : CharacterController
 {
+    //object creation 
+    private Animator animator;
 
     //game variables
     bool enemyCollideFlag = false;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         calculateCharacterStart();
         setMaxHealth(10f);
     }
@@ -48,11 +51,20 @@ public class SpaceManController : CharacterController
     int inputSystemToGetAxis()
     {
         if (Keyboard.current.aKey.isPressed)
+        {
+            animator.SetBool("Walk", true);
             return -1;
+        }
         if (Keyboard.current.dKey.isPressed)
+        {
+            animator.SetBool("Walk", true);
             return 1;
+        }
         else
+        {
+            animator.SetBool("Walk", false);
             return 0;
+        }
     }
 
     int lookLeftOrRight()
