459843611d4c4b42971b982305e9cb1310004178
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2025-04-01 23:41:59 -0500

    push before bed

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
+using System.Linq;
 
 public class UIHandler : MonoBehaviour
 {
+    //game variables
     public float currentHealth = 1f;
-    public static UIHandler instance { get; private set; }
+    public float movePercent = 0f;
     private float fadeCounter = 180f;
-    private bool escapeClicked = false;
-    private float lastEscapePress = 0f;
     private float parentTop = 0f;
+    private bool escapeClicked = false;
+    private bool eClicked = false;
+    private bool coroutineRunning = false;
     public float speed = .05f;
     public float shiftNum = 4f;
+    
+    //object creation
+    public static UIHandler instance { get; private set; }
+
     private VisualElement fullBar;
     private VisualElement fullFuelBar;
     private VisualElement warningBar;
     private VisualElement pauseMenu;
     private VisualElement darken;
     private VisualElement overlayContainer;
+    private VisualElement pauseContainer;
+    private VisualElement objectiveContainer;
+    private VisualElement offScreenObjectivesContainer;
+    private VisualElement killEnemyObjective;
     private VisualElement exitGameButton;
     private VisualElement[] overlayArray = new VisualElement[25];
+    private List<VisualElement> objectivesList;
+
     private InputSystemHelper escapeKey;
+    private InputSystemHelper eKey;
+
     private Coroutine moveScanLinesCoroutine;
 
     private void Awake()
@@ -34,23 +49,32 @@ public class UIHandler : MonoBehaviour
     void Start()
     {
         UIDocument uiDocument = GetComponent<UIDocument>();
+
         fullBar = uiDocument.rootVisualElement.Q<VisualElement>("healthBar");
         fullFuelBar = uiDocument.rootVisualElement.Q<VisualElement>("fuelBar");
         warningBar = uiDocument.rootVisualElement.Q<VisualElement>("warningBar");
         pauseMenu = uiDocument.rootVisualElement.Q<VisualElement>("pause");
+        pauseContainer = uiDocument.rootVisualElement.Q<VisualElement>("pauseContainer");
         darken = uiDocument.rootVisualElement.Q<VisualElement>("darken");
         exitGameButton = uiDocument.rootVisualElement.Q<Button>("exitGameButton");
-
+        objectiveContainer = uiDocument.rootVisualElement.Q<VisualElement>("text");
+        killEnemyObjective = uiDocument.rootVisualElement.Q<VisualElement>("killEnemy");
         overlayContainer = uiDocument.rootVisualElement.Q<VisualElement>("overlayContainer");
+        offScreenObjectivesContainer = uiDocument.rootVisualElement.Q<VisualElement>("offScreenObjectives");
         overlayArray = overlayContainer.Query<VisualElement>("overlay").ToList().ToArray();
-
+        Debug.Log($"Number of children: {offScreenObjectivesContainer.Children().Count()}");
+        foreach (VisualElement child in offScreenObjectivesContainer.Children())
+            objectivesList.Add(child);
         warningBar.style.opacity = 0f;
+        pauseContainer.style.opacity = 0f;
+        objectiveContainer.style.opacity = 0f;
         pauseMenu.style.transitionDuration = new List<TimeValue> { new TimeValue(0.25f, TimeUnit.Second) };
         escapeKey = new InputSystemHelper(Keyboard.current.escapeKey);
+        eKey = new InputSystemHelper(Keyboard.current.eKey);
         pauseMenu.style.top = Length.Percent(110);
 
-        exitGameButton.RegisterCallback<ClickEvent>(exitGame);
-
+        exitGameButton.RegisterCallback<ClickEvent>(exitGame); //gotta figure this out
+        objectiveContainer.style.backgroundImage = objectivesList[0].resolvedStyle.backgroundImage;
     }
 
     // Update is called once per frame
@@ -60,16 +84,16 @@ public class UIHandler : MonoBehaviour
         {
             warningBarFunction();
         }
-        if (escapeKey.wasPressedWithCooldown())
+        if (escapeKey.wasPressedWithCooldown()&&!eClicked)
         {
-            lastEscapePress = Time.unscaledTime;
             escapeClicked = !escapeClicked;
             if (escapeClicked)
             {
-                Time.timeScale = 0;
+                //Time.timeScale = 0; //this pauses the scan lines too for some reason
                 pauseMenu.style.top = Length.Percent(0);
                 darken.style.backgroundColor = new Color(0, 0, 0, 0.7f);
                 moveScanLinesCoroutine = StartCoroutine(shiftOverlayRoutine());
+                pauseContainer.style.opacity = 1;
             }
             else
             {
@@ -77,6 +101,23 @@ public class UIHandler : MonoBehaviour
                 pauseMenu.style.top = Length.Percent(110);
                 darken.style.backgroundColor = new Color(0, 0, 0, 0.0f);
                 StopCoroutine(moveScanLinesCoroutine);
+                pauseContainer.style.opacity = 0;
+            }
+        }
+        if (eKey.wasPressedWithCooldown()&&!escapeClicked)
+        {
+            eClicked = !eClicked;
+            if (eClicked)
+            {
+                pauseMenu.style.top = Length.Percent(movePercent);
+                moveScanLinesCoroutine = StartCoroutine(shiftOverlayRoutine());
+                objectiveContainer.style.opacity = 1;
+            }
+            else
+            {
+                pauseMenu.style.top = Length.Percent(110);
+                StopCoroutine(moveScanLinesCoroutine);
+                objectiveContainer.style.opacity = 0;
             }
         }
     }
@@ -85,6 +126,7 @@ public class UIHandler : MonoBehaviour
     {
         while (true)
         {
+
             for (int i = 0; i < 25; i++)
             {
                 parentTop = overlayContainer.resolvedStyle.height;
