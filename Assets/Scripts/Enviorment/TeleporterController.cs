7d9758cfea0feb314bcab213561c5e6c2c4b1126
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2025-04-15 23:50:58 -0500

    push before bed

--- Assets/Scripts/Enviorment/TeleporterController.cs
+++ Assets/Scripts/Enviorment/TeleporterController.cs
@@ -8,20 +8,28 @@ public class TeleporterController : MonoBehaviour
     [SerializeField] float lineHeight = 1f;
     [SerializeField] float loopTime = .1f;
     [SerializeField] float startPoint = 2.5f;
+    [SerializeField] float lineCount = 5;
     [SerializeField] bool toggleState = false;
     [SerializeField] Sprite onSprite;
     [SerializeField] Sprite offSprite;
+    [SerializeField] GameObject line;
+    [SerializeField] GameObject transportBeam;
     private SpriteRenderer sr;
     private List<SpriteRenderer> srLines = new List<SpriteRenderer>();
     private Coroutine moveLinesCoroutine;
+    private bool trigger = false;
+    private bool on = false;
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        sr.sprite = offSprite;
-        foreach (Transform line in transform)
+        srLines.Add(line.GetComponent<SpriteRenderer>());
+        for (int i = 0; i < lineCount; i++)
         {
-            srLines.Add(line.gameObject.GetComponent<SpriteRenderer>());
+            Transform lineHolder = transform.GetChild(0);
+            GameObject newLine = Instantiate(line, lineHolder); // Parent first
+            newLine.transform.localPosition = line.transform.localPosition + new Vector3(-0.3375332f, i * 0.5f, 0f);
+            srLines.Add(newLine.GetComponent<SpriteRenderer>());
         }
     }
 
@@ -45,8 +53,18 @@ public class TeleporterController : MonoBehaviour
             {
                 sr.sprite = onSprite;
                 moveLinesCoroutine = StartCoroutine(moveLines());
+                on = true;
             }
+            on = !on; //reverses the state of the teleporter on or off
             toggleState = false;
+        }
+
+        if (trigger && on) //redo this get the "getcomponent" out
+        {
+            SpriteRenderer transportBeammRenderer = transportBeam.GetComponent<SpriteRenderer>();
+            Color c = transportBeammRenderer.color;
+            c.a = 1f;
+            transportBeammRenderer.color = c;
         }
     }
 
@@ -54,20 +72,20 @@ public class TeleporterController : MonoBehaviour
     {
         while (true)
         {
-            for (int i = 0; i < transform.childCount; i++)
+            for (int i = 0; i < transform.GetChild(0).childCount; i++)
             {
-                Transform line = transform.GetChild(i);
+                Transform line = transform.GetChild(0).GetChild(i);
                 SpriteRenderer srLine = srLines[i];
                 line.localPosition = line.localPosition + new Vector3(0f, lineMoveAmount, 0f);
                 Color c = srLine.color;
                 float distance = Mathf.Abs(line.localPosition.y - startPoint);
 
-                float falloff = Mathf.Log10(lineHeight + 1) - Mathf.Log10(distance + 1);
+                float falloff = Mathf.Log10(lineHeight + 1+2) - Mathf.Log10(distance + 1);
                 float maxFalloff = Mathf.Log10(lineHeight + 1); // Max possible falloff
                 c.a = Mathf.Clamp01(falloff / maxFalloff);
                 if (line.localPosition.y >= lineHeight)
                 {
-                    line.localPosition = new Vector3(0f, startPoint, 0f);
+                    line.localPosition = new Vector3(-0.3375332f, startPoint, 0f);
                     c.a = 1f;
                 }
                 srLine.color = c;
@@ -79,5 +97,10 @@ public class TeleporterController : MonoBehaviour
     public void toggleStateFunc()
     {
         toggleState = true;
+    }
+
+    public void setTransportTrigger(bool trigger)
+    {
+        this.trigger = trigger;
     }
 }
---
+++ Assets/Scripts/Enviorment/TeleporterTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject teleporterObject;
+    private TeleporterController teleporterController;
+
+    void Start()
+    {
+        teleporterController = teleporterObject.GetComponent<TeleporterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.name == "SpaceMan")
+            teleporterController.setTransportTrigger(true);
+        else
+            teleporterController.setTransportTrigger(false);
+    }
+}
