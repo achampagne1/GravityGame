642c612517117b658d1791fa7062d187f3cd86c0
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2025-07-22 20:16:32 -0500

    added more for shield

--- Assets/Scripts/Characters/ShieldTriggerController.cs
+++ Assets/Scripts/Characters/ShieldTriggerController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ShieldTriggerController : TriggerBoundaryCotroller
 {
     [SerializeField] GameObject shieldHit;
+    [SerializeField] float fadeSize = .1f;
     private CharacterController parentController;
+    private CircleCollider2D collider;
     private float shieldStrength = 10f;
     // Start is called before the first frame update
     public override void Start()
     {
         parentController = transform.parent.gameObject.GetComponent<CharacterController>();
+        collider = GetComponent<CircleCollider2D>();
         base.Start();
     }
 
@@ -28,8 +31,8 @@ public class ShieldTriggerController : TriggerBoundaryCotroller
         {
             if (shieldStrength > 0)
             {
-                StartCoroutine(spawnShieldHit());
-                shieldStrength -= 1;
+                StartCoroutine(spawnShieldHit(trigger.gameObject));
+                //shieldStrength -= 1; uncomment later
                 if (shieldStrength < 0)
                     shieldStrength = 0;
             }
@@ -37,11 +40,36 @@ public class ShieldTriggerController : TriggerBoundaryCotroller
         }
     }
 
-    private IEnumerator spawnShieldHit()
+    private IEnumerator spawnShieldHit(GameObject trigger)
     {
-        GameObject shieldHitTemp = Instantiate(shieldHit, transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(.5f);
+        var (strikeLocation,rotation) = determineStrikeLocation(trigger);
+        GameObject shieldHitTemp = Instantiate(shieldHit, new Vector3(strikeLocation.x,strikeLocation.y,transform.position.z), rotation);
+        shieldHitTemp.transform.SetParent(transform);
+        SpriteRenderer shieldHitTempSR = shieldHitTemp.GetComponent<SpriteRenderer>();
+        SparkTrigger sparkTrigger = shieldHitTemp.GetComponent<SparkTrigger>();
+        sparkTrigger.triggerSparks();
+        float opacity = 1;
+        while(opacity > 0)
+        {
+            HelperFunctions.changeOpacity(shieldHitTempSR, opacity-=fadeSize);
+            yield return new WaitForSeconds(.05f);
+        }
         GameObject.Destroy(shieldHitTemp);
         yield return null;
+    }
+
+    private (Vector2,Quaternion) determineStrikeLocation(GameObject trigger) 
+    {
+        Vector2 triggerVelocity = (Vector2)trigger.GetComponent<Rigidbody2D>().velocity;
+        Vector2 triggerLocationCurrent = (Vector2)trigger.transform.position;
+        Vector2 triggerLocationPrevious = triggerLocationCurrent - triggerVelocity;
+        Vector2 worldCenter = (Vector2)transform.position + collider.offset;
+        float worldRadius = collider.radius * Mathf.Max(transform.lossyScale.x, transform.lossyScale.y);
+        Vector2 intersection = new Vector2();
+        bool found = HelperFunctions.chordIntersection(triggerLocationPrevious, triggerLocationCurrent, worldCenter, worldRadius, out intersection);
+
+        float angle = Mathf.Atan2((intersection.y - worldCenter.y), (intersection.x - worldCenter.x)) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+        return (intersection,rotation);
     }
 }
--- Assets/Scripts/HelperFunctions.cs
+++ Assets/Scripts/HelperFunctions.cs
@@ -34,10 +34,53 @@ public struct HelperFunctions
         return new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
     }
 
+    //used to abstract some of the smaller stuff for changing opacity
     public static void changeOpacity(SpriteRenderer sr,float opacity)
     {
         Color color = sr.color;
         color.a = opacity;
         sr.color = color;
+    }
+
+    //can be used to find the intersection between a chord and a circle. good for finding strile locations for bulelts
+    //written by chat gpt
+    public static bool chordIntersection(Vector2 p1, Vector2 p2, Vector2 circleCenter, float radius, out Vector2 intersection) 
+    {
+        intersection = Vector2.zero;
+
+        Vector2 d = p2 - p1;                   // Direction vector of the line
+        Vector2 f = p1 - circleCenter;         // Vector from circle center to p1
+
+        float a = Vector2.Dot(d, d);
+        float b = 2 * Vector2.Dot(f, d);
+        float c = Vector2.Dot(f, f) - radius * radius;
+
+        float discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0)
+        {
+            // No intersection
+            return false;
+        }
+
+        discriminant = Mathf.Sqrt(discriminant);
+
+        float t1 = (-b - discriminant) / (2 * a);
+        float t2 = (-b + discriminant) / (2 * a);
+
+        bool found = false;
+
+        if (t1 >= 0 && t1 <= 1)
+        {
+            intersection = p1 + t1 * d;
+            found = true;
+        }
+        else if (t2 >= 0 && t2 <= 1)
+        {
+            intersection = p1 + t2 * d;
+            found = true;
+        }
+
+        return found;
     }
 }
