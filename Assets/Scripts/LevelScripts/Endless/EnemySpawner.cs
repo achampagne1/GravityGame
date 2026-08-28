4ed5ea28c079edc1ca713ead6983173d521f2ef0
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2026-08-28 10:44:14 -0400

    changed spawn rate to an s curve

--- Assets/Scripts/LevelScripts/Endless/EnemySpawner.cs
+++ Assets/Scripts/LevelScripts/Endless/EnemySpawner.cs
@@ -12,12 +12,9 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject greenTeam;
     [SerializeField] private GameObject bug;
     [SerializeField] private GameObject drone;
+    [SerializeField] private float levelShift = .5f;
+    [SerializeField] private float rampIn = .5f;
     [SerializeField] private float greenTeamGain = .2f;
-    [SerializeField] private float bugGain = .5f;
-    [SerializeField] private float droneGain = .5f;
-    [SerializeField] private float greenTeamStart = .2f;
-    [SerializeField] private float bugStart = .7f;
-    [SerializeField] private float droneStart = .1f;
 
 
     //private variables
@@ -58,17 +55,22 @@ public class EnemySpawner : MonoBehaviour
 
     private int spawnChance(int level)
     {
+        //spawn rate is an s curve
+        //levelshift adjusts the cross over point between drones and bugs
+        //rampin defines how lazily it reaches that cross over
+        //greenTeamChance is constant
         double randomNum = (double)(UnityEngine.Random.Range(1, 101))/100f;
-
-        double t = 1.0 - Math.Exp(-level / 10.0);
-
-        double droneChance = droneStart + t * droneGain;
-
-        double bugChance = 1.0 - droneChance - greenTeamStart;
+        double greenTeamChance = greenTeamGain;
+        double droneChance = 1/(1+ Math.Exp(levelShift*rampIn-(rampIn/10)*level));
+        double bugChance = 1 - droneChance;
+        droneChance *= 1-greenTeamChance;
+        bugChance *= 1 - greenTeamChance;
+        //make 3 bands, one for bug, one for green, one for drone
+        //chances should always add up to 1
 
         if (randomNum < bugChance)
             return 0;
-        else if (randomNum < bugChance + greenTeamStart)
+        else if (randomNum < bugChance + greenTeamChance)
             return 1;
         else 
             return 2;
